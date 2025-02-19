7df92180d007f3f70092d1404a55daae2e7983a7
Guilherme-d3v <guii.parada@gmail.com>
2025-02-19 20:05:24 -0300

    atrt

--- models.py
+++ models.py
@@ -39,3 +39,11 @@ class Review(db.Model):
     rating = db.Column(db.Integer, nullable=False)
     comment = db.Column(db.String(200))
     product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.email}')"
