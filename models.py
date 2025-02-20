3f23fb5b8d6169c1952bfa42467854671c93841b
Guilherme-d3v <guii.parada@gmail.com>
2025-02-20 16:28:55 -0300

    att

--- models.py
+++ models.py
@@ -8,6 +8,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(60), nullable=False)
+    nome = db.Column(db.String(100), nullable=False)
+
 
     def __repr__(self):
         return f"User('{self.email}')"
