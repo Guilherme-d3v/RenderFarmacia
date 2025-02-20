451e6f8cc75d455896dc7a126814f329ef616d72
Guilherme-d3v <guii.parada@gmail.com>
2025-02-20 00:15:21 -0300

    att

--- models.py
+++ models.py
@@ -21,7 +21,7 @@ class User(db.Model):
 class CartItem(db.Model):
     __tablename__ = 'cart_items'
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
     quantity = db.Column(db.Integer, nullable=False, default=1)
 
